fbf8506bb3210c22c542cb77417dd94a3d50af4b
solareon <solareon@users.noreply.github.com>
2026-01-25 13:32:56 +0000

    chore: bump manifest version to v1.0.6

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,13 @@
 fx_version 'cerulean'
 game 'gta5'
-version '1.0.5'
+version '1.0.6'
 
 lua54 'yes'
 
 title 'SLRN Multijob'
 description 'QB-Core multijob application for LB-Phone'
 author 'solareon.'
-version '1.0.5'
+version '1.0.6'
 
 shared_scripts {
     '@ox_lib/init.lua',
